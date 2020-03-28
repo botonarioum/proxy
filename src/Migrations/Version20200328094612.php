1a980d1169caac7df6bbb5e9370c2cc4cb3a56f9
Ihor Kopyl <lov3catch@gmail.com>
2020-03-28 11:48:12 +0200

    fix migration

--- src/Migrations/Version20200328094612.php
+++ src/Migrations/Version20200328094612.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200328091201 extends AbstractMigration
+final class Version20200328094612 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -23,7 +23,10 @@ final class Version20200328091201 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
 
         $this->addSql('CREATE SEQUENCE bot_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE data_lake_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE TABLE bot (id INT NOT NULL, token VARCHAR(255) NOT NULL, telegram_user_id INT NOT NULL, telegram_bot_id INT NOT NULL, is_enable BOOLEAN NOT NULL, comment TEXT NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE data_lake (id INT NOT NULL, bot_id INT NOT NULL, data TEXT NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('COMMENT ON COLUMN data_lake.data IS \'(DC2Type:json)\'');
     }
 
     public function down(Schema $schema) : void
@@ -33,6 +36,8 @@ final class Version20200328091201 extends AbstractMigration
 
         $this->addSql('CREATE SCHEMA public');
         $this->addSql('DROP SEQUENCE bot_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE data_lake_id_seq CASCADE');
         $this->addSql('DROP TABLE bot');
+        $this->addSql('DROP TABLE data_lake');
     }
 }
