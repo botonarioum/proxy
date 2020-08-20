3837a6bfde30a81c22f615bcf6273e8d36d32601
Ihor Kopyl <ihor.kopyl@alkima.de>
2020-08-20 11:25:21 +0300

    sandbox command

---
+++ src/Command/SandboxCommand.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Command;
+
+use App\Messages\RedirectThisMessage;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Messenger\MessageBusInterface;
+
+class SandboxCommand extends Command
+{
+    protected static $defaultName = 'app:tmp:sandbox';
+    /**
+     * @var MessageBusInterface
+     */
+    private MessageBusInterface $bus;
+
+    public function __construct(string $name = null, MessageBusInterface $bus)
+    {
+        parent::__construct($name);
+
+        $this->bus = $bus;
+    }
+
+
+    protected function configure()
+    {
+        $this
+            ->setDescription('Add a short description for your command')
+            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
+            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $message = new RedirectThisMessage('redirect-here-url', []);
+
+        $this->bus->dispatch($message);
+
+
+
+        $io = new SymfonyStyle($input, $output);
+        $arg1 = $input->getArgument('arg1');
+
+        if ($arg1) {
+            $io->note(sprintf('You passed an argument: %s', $arg1));
+        }
+
+        if ($input->getOption('option1')) {
+            // ...
+        }
+
+        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+
+        return 0;
+    }
+}
